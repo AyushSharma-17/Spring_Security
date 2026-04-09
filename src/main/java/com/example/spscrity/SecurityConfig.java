6888f20cd308e963b92199e30c4d049ddeddc138
ayush <ayushsharma12345qwerty@gmail.com>
2026-04-09 13:15:36 +0530

    add custom security where user is stored in springboot database in UserDetailsService() but it is not safe

--- src/main/java/com/example/spscrity/SecurityConfig.java
+++ src/main/java/com/example/spscrity/SecurityConfig.java
@@ -6,19 +6,35 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http
-                .csrf(customizer-> customizer.disable())
-                .authorizeHttpRequests(request->request.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+//        return http
+//                .csrf(customizer-> customizer.disable())
+//                .authorizeHttpRequests(request->request.anyRequest().authenticated())
+//                .httpBasic(Customizer.withDefaults())
+//                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .build();
+//    }
         //http.formLogin(Customizer.withDefaults());  this is used to disable the form login and let access throught the postman
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1= User
+                .withDefaultPasswordEncoder()  // method depicated
+                .username("ayush")
+                .password("ayush@123")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
     }
 }
